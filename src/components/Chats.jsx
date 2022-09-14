fcb9758aaef4d0c55ff2babb6fdfc8c1b1510ac5
Nicholas Anich <NicholasAnich.infinitevolume@gmail.com>
2022-09-14 11:25:39 -0700

    implemented chats barebones feature that displays multiple users

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Chats = () => {
+  const pp =
+    "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60";
   return (
-    <div>Chats</div>
-  )
-}
+    <div className="chats">
+      <div className="userChat">
+        <img src={pp} alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>hello!</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={pp} alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>hello!</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={pp} alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>hello!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chats
+export default Chats;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
+  const pp =
+    "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60";
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Find a user" />
+      </div>
+      <div className="userChat">
+        <img src={pp} alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
