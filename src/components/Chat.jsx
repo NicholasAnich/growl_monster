f935c53244a4a26d95e87f911e0d50140c32dde5
Nicholas Anich <NicholasAnich.infinitevolume@gmail.com>
2022-09-14 13:29:01 -0700

    created message component with mock data/images and stylings

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import Cam from "../img/cam.png"
+import Add from "../img/add.png"
+import More from "../img/more.png"
+import Messages from "./Messages";
+import Input from "./Input";
 
 const Chat = () => {
   return (
     <div className="chat">
-      Chat
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="cam feature icon" />
+          <img src={Add} alt="add feature icon" />
+          <img src={More} alt="more feature icon" />
+        </div>
+      </div>
+        <Messages />
+        <Input />
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
