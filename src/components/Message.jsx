2577b9451f0140107681dbecb4fa6f82a543544a
Nicholas Anich <NicholasAnich.infinitevolume@gmail.com>
2022-09-15 11:54:49 -0700

    created firebase.js file, added firebase Auth and cloud Storage functions to Register

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,7 +4,7 @@ const Message = () => {
   const pp =
     "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60";
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
         <img src={pp} alt="" />
         <span>just now</span>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIaAqMgssHBSitW0Lyzq7sKPw8CFBh3NM",
+  authDomain: "growlmonsterchat.firebaseapp.com",
+  projectId: "growlmonsterchat",
+  storageBucket: "growlmonsterchat.appspot.com",
+  messagingSenderId: "886482564508",
+  appId: "1:886482564508:web:a5e4a4fe93a2d1ac568459",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
